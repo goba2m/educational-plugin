84bed510f26b99bea6323ba48fdb1d659910fd04
Zinaida.Smirnova <Zinaida.Smirnova@jetbrains.com>
2023-07-15 14:21:28 +0400

    EDU-6088 cleanup

--- educational-core/src/com/jetbrains/edu/learning/actions/LeaveInIdeFeedbackAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/actions/LeaveInIdeFeedbackAction.kt
@@ -1,9 +1,11 @@
 package com.jetbrains.edu.learning.actions
 
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.RightAlignedToolbarAction
 import com.intellij.openapi.project.DumbAwareAction
 import com.jetbrains.edu.EducationalCoreIcons
+import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
 import com.jetbrains.edu.learning.actions.EduActionUtils.getCurrentTask
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.uIfeedback.InIdeFeedbackDialog
@@ -22,7 +24,7 @@ class LeaveInIdeFeedbackAction : DumbAwareAction(
     if (dialog.showAndGet()) {
       dialog.showThanksNotification()
     }
-
+    //TODO: add statistics
     //EduCounterUsageCollector.leaveFeedback()
   }
 
@@ -30,10 +32,14 @@ class LeaveInIdeFeedbackAction : DumbAwareAction(
     e.presentation.isEnabledAndVisible = false
 
     val project = e.project ?: return
-//    if (!project.isStudentProject()) return
-    val task = project.getCurrentTask() ?: return
-    //val course = task.course
+    if (!project.isStudentProject()) return
+    project.getCurrentTask() ?: return
+
     e.presentation.isEnabledAndVisible = true
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread {
+    return ActionUpdateThread.EDT
   }
 
   companion object {
