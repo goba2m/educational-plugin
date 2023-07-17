cbbe6a9b3e3c128b0b7867244f787488106bcf6c
Mark Prudnikov <mark.prudnikov@jetbrains.com>
2023-07-17 18:32:56 +0200

    improved ShareMySolutionsAction#update

--- educational-core/src/com/jetbrains/edu/learning/marketplace/actions/ShareMySolutionsAction.kt
+++ educational-core/src/com/jetbrains/edu/learning/marketplace/actions/ShareMySolutionsAction.kt
@@ -1,21 +1,28 @@
 package com.jetbrains.edu.learning.marketplace.actions
 
+import com.intellij.execution.process.ProcessIOExecutorService
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareToggleAction
 import com.intellij.openapi.util.registry.Registry
 import com.jetbrains.edu.learning.EduUtilsKt.isStudentProject
+import com.jetbrains.edu.learning.marketplace.api.MarketplaceSubmissionsConnector
 import com.jetbrains.edu.learning.marketplace.isMarketplaceCourse
 import com.jetbrains.edu.learning.marketplace.settings.MarketplaceSettings
 import com.jetbrains.edu.learning.messages.EduCoreBundle
 import org.jetbrains.annotations.NonNls
+import java.util.concurrent.CompletableFuture
 
 class ShareMySolutionsAction : DumbAwareToggleAction(EduCoreBundle.message("marketplace.solutions.sharing.action")) {
 
     override fun update(e: AnActionEvent) {
         super.update(e)
         val project = e.project ?: return
-        e.presentation.isEnabled = MarketplaceSettings.INSTANCE.solutionsSharing != null
+
+        CompletableFuture.runAsync({
+            e.presentation.isEnabled = isAvailableInSettings() || MarketplaceSubmissionsConnector.getInstance().getSharingPreference() != null
+        }, ProcessIOExecutorService.INSTANCE)
+
         e.presentation.isVisible =
           project.isMarketplaceCourse()
           && project.isStudentProject()
@@ -27,6 +34,8 @@ class ShareMySolutionsAction : DumbAwareToggleAction(EduCoreBundle.message("mark
     override fun setSelected(e: AnActionEvent, state: Boolean) = MarketplaceSettings.INSTANCE.updateSharingPreference(state)
 
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    private fun isAvailableInSettings() = MarketplaceSettings.INSTANCE.solutionsSharing != null
 
     companion object {
 
