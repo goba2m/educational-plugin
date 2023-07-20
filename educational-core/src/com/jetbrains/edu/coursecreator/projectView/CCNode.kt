72b49eb3bea3f97aa766029ee232fa2aa4630f9d
Ilia Posov <ilia.posov@jetbrains.com>
2023-07-20 15:13:43 +0200

    EDU-6072: Show excluded directories in Course view (not only files)

--- educational-core/src/com/jetbrains/edu/coursecreator/projectView/CCNode.kt
+++ educational-core/src/com/jetbrains/edu/coursecreator/projectView/CCNode.kt
@@ -1,17 +1,21 @@
 package com.jetbrains.edu.coursecreator.projectView
 
+import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.projectView.impl.nodes.PsiDirectoryNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
+import com.intellij.ui.SimpleTextAttributes
+import com.jetbrains.edu.coursecreator.AdditionalFilesUtils
 import com.jetbrains.edu.coursecreator.CCUtils.isCourseCreator
 import com.jetbrains.edu.learning.EduUtilsKt
 import com.jetbrains.edu.learning.StudyTaskManager
 import com.jetbrains.edu.learning.courseFormat.ext.configurator
 import com.jetbrains.edu.learning.courseFormat.tasks.Task
 import com.jetbrains.edu.learning.isTestsFile
+import com.jetbrains.edu.learning.messages.EduCoreBundle
 import com.jetbrains.edu.learning.projectView.DirectoryNode
 
 class CCNode(
@@ -20,6 +24,17 @@ class CCNode(
   viewSettings: ViewSettings,
   task: Task?
 ) : DirectoryNode(project, value, viewSettings, task) {
+
+  private val excludedName: String?
+  init {
+    // show node as excluded, if it is not inside a task folder, but is in the .courseignore
+    excludedName = if (task == null && AdditionalFilesUtils.inCourseIgnore(value.virtualFile, project)) {
+      EduCoreBundle.message("course.creator.course.view.excluded", value.name)
+    }
+    else {
+      null
+    }
+  }
 
   override fun canNavigate(): Boolean = true
 
@@ -49,6 +64,14 @@ class CCNode(
       }
     }
     return null
+  }
+
+  override fun updateImpl(data: PresentationData) {
+    super.updateImpl(data)
+    if (excludedName != null) {
+      data.clearText()
+      data.addText(excludedName, SimpleTextAttributes.GRAY_ATTRIBUTES)
+    }
   }
 
   override fun createChildDirectoryNode(value: PsiDirectory): PsiDirectoryNode {
